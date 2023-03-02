a3fcb0a9ac200d0bbc00fdd7dc80e12f61163939
agp19d <agp19d@gmail.com>
2023-03-02 17:56:59 -0500

    added multiplication.js file

---
+++ multiplication.js
@@ -0,0 +1,14 @@
+// Function that outputs the multiplication table of a number to the console
+function multiplicationTable(num) {
+  for (let i = 1; i <= 10; i++) {
+    let row = "";
+    for (let j = 1; j <= 10; j++) {
+      let result = num * (i + (j - 1));
+      row += result.toString().padStart(4, " ");
+    }
+    console.log(row);
+  }
+}
+
+// Testing the function with the multiplication table of 10
+multiplicationTable(10);
